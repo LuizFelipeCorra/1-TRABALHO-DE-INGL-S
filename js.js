c358bdce5ff23b8ba71b0c60303ca6ec0710cab5
Felipe Correa <luizfelipelopescorrea@gmail.com>
2022-07-07 18:48:52 -0300

    1º TRABALHO DE INGLÊS

---
+++ js.js
@@ -0,0 +1,17 @@
+//Crie 3 variáveis, cada uma com um array e dentro de cada array, insira 3 (TRÊS) Objetos, contendo: 
+//title, director e year.
+let comedy = [
+    {MinhaMãeéUmaPeça},
+    {SusanaGarcia,AndréPellenz,CésarRodrigues},
+    {Year2013}
+]
+let action = [
+    {vvingadoresUltimato},
+    { AnthonyRusso,JoeRusso},
+    {Year2019}
+]
+let fantasy = [
+    {DoutorEstranhoNoMultiversoDaLoucura},
+    {SamRaimi},
+    {Year2022}
+]
